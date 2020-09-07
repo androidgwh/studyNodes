522a1402d0f2c7a47f80c163c04750a482a66128
guowenhui <guowenhui@aerozhonghuan.com>
2020-09-07 18:03:22 +0800

    添加自定义相机

--- app/src/main/java/com/gwh/studyplan/MainActivity.java
+++ app/src/main/java/com/gwh/studyplan/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,CallActivity.class));
+                startActivity(new Intent(MainActivity.this,CameraActivity.class));
             }
         });
     }
